24943b99e5f3b6e3e999dd0bbad3244212777526
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-04-26 18:28:56 +0100

    Update src/data/tags.py

--- src/data/tags.py
+++ src/data/tags.py
@@ -11,19 +11,23 @@ class Tags:
     Examine data balance/imbalance by tag frequency.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, data: pd.DataFrame) -> None:
         """
         Constructor
         """
-        
+
+        self.__tag_fields: list[str] = ['tag', 'annotation', 'category']
+        self.__tag_data: pd.DataFrame = data.copy()[self.__tag_fields]
+
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger: logging.Logger = logging.getLogger(name=__name__)
 
-    def exc(self, blob: pd.DataFrame):
+    def exc(self):
 
-        elements: pd.DataFrame = blob.groupby(by=['tag', 'annotation', 'category']).value_counts().to_frame().reset_index(drop=False)
+        elements: pd.DataFrame = self.__tag_data.groupby(by=self.__tag_fields).value_counts().to_frame()
+        elements.reset_index(drop=False, inplace=True)
 
-        self.__logger.info(elements)
+        self.__logger.info(msg=elements)
