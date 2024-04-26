165f705f7c7cb54376c2465b69aac30bfc7bd278
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-04-26 19:09:40 +0100

    Update src/data/tags.py

--- src/data/tags.py
+++ src/data/tags.py
@@ -2,6 +2,7 @@
 import logging
 import numpy as np
 import pandas as pd
+import typing
 
 import config
 
@@ -30,7 +31,7 @@ class Tags:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger: logging.Logger = logging.getLogger(name=__name__)
 
-    def __categories(self, blob: pd.DataFrame) -> np.ndarray:
+    def __viable(self, blob: pd.DataFrame) -> pd.DataFrame:
         """
         
         :param blob: A frame that includes {category}, and {count} per category
@@ -42,13 +43,36 @@ class Tags:
         categories = categories.copy().loc[categories['count'] >= self.__mcf, :]
         self.__logger.info(msg=categories)
 
-        return categories['category'].values
+        elements: pd.DataFrame = blob.copy().loc[
+            blob['category'].isin(values=categories['category'].values), :]
+        elements.sort_values(by='tag', inplace=True)
 
-    def exc(self):
+        return elements
+    
+    def __coding(self, series: pd.Series) -> typing.Tuple[dict, dict]:
 
+        enumerator: dict[str, int] = {k: v for v, k in enumerate(iterable=series)}
+
+        denumerator: dict[int, str] = {v: k for v, k in enumerate(iterable=series)}
+
+        return enumerator, denumerator
+
+
+    def exc(self) -> typing.Tuple[pd.DataFrame, dict, dict]:
+        """"
+        
+        """
+
+        # Frequencies
         elements: pd.DataFrame = self.__tag_data.groupby(by=self.__tag_fields).value_counts().to_frame()
         elements.reset_index(drop=False, inplace=True)
 
-        categories = self.__categories(blob=elements)
+        # Focusing on viable categories
+        elements = self.__viable(blob=elements)
 
-        self.__logger.info(msg=elements)
+        # Coding
+        enumerator: dict
+        denumerator: dict
+        enumerator, denumerator = self.__coding(series=elements['tag'])
+
+        return elements, enumerator, denumerator
