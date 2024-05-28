1d23d5263921fb4fd69a483f8de1cdefe15db569
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-28 20:19:54 +0100

    Add src/structures/bert/parameters.py

---
+++ src/structures/bert/parameters.py
@@ -0,0 +1,10 @@
+
+import transformers
+
+class Parameters:
+
+    def __init__(self):
+
+        # Bases
+        self.tokenizer = transformers.BertTokenizerFast.from_pretrained(
+            pretrained_model_name_or_path='google-bert/bert-base-uncased')
