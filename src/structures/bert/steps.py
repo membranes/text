7422d7e954af7e18a409d43ea63b040772bdbb3f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 18:13:27 +0100

    Update src/structures/bert/steps.py

--- src/structures/bert/steps.py
+++ src/structures/bert/steps.py
@@ -2,8 +2,11 @@ import logging
 
 import pandas as pd
 
+import torch.utils.data as tu
+
 import src.elements.variable
 import src.structures.bert.dataset
+import src.structures.loadings
 
 
 class Steps:
@@ -21,6 +24,9 @@ class Steps:
         self.__training = training
         self.__validating = validating
 
+        # Instances
+        self.__loadings = src.structures.loadings.Loadings()
+
         # A set of values for machine learning model development
         self.__variable = src.elements.variable.Variable()
         self.__variable._replace(EPOCHS=2)
@@ -31,18 +37,29 @@ class Steps:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def __structure(self, blob: pd.DataFrame) -> dict:
+    def __structure(self, blob: pd.DataFrame) -> tu.Dataset:
+        """
 
-        dataset = src.structures.bert.dataset.Dataset(blob, self.__variable, self.__enumerator)
+        :param blob:
+        :return:
+        """
+
+        dataset: tu.Dataset = src.structures.bert.dataset.Dataset(blob, self.__variable, self.__enumerator)
         self.__logger.info('frame: %s', blob.shape)
-        self.__logger.info('dataset: %s', dataset.__len__())
+        self.__logger.info('dataset: %s', dataset.__doc__)
 
         return dataset
 
     def exc(self):
 
         self.__logger.info('Training')
-        self.__structure(blob=self.__training)
+        training_dataset = self.__structure(blob=self.__training)
+        training_loader = self.__loadings.exc(training_dataset, parameters={
+            'batch_size': self.__variable.TRAIN_BATCH_SIZE, 'shuffle': True, 'num_workers': 0})
+
+
 
         self.__logger.info('Validating')
-        self.__structure(blob=self.__validating)
+        validating_dataset = self.__structure(blob=self.__validating)
+        validating_loader = self.__loadings.exc(validating_dataset, parameters={
+            'batch_size': self.__variable.VALID_BATCH_SIZE, 'shuffle': True, 'num_workers': 0})
