b94ed6dcb5c453df194b719b5a23d908cfedc5d4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 01:54:16 +0100

    Update src/elements/variable.py

--- src/elements/variable.py
+++ src/elements/variable.py
@@ -1,6 +1,29 @@
 import typing
 
 class Variable(typing.NamedTuple):
+    """
+
+    Attributes
+    ----------
+
+    MAX_LENGTH : int
+        The maximum number of tokens
+
+    TRAIN_BATCH_SIZE : int
+        The training stage data batch size
+
+    VALID_BATCH_SIZE : int
+        The validation stage data batch size
+
+    EPOCHS : int
+        The number of epochs
+
+    LEARNING_RATE : float
+        The learning rate
+
+    MAX_GRADIENT_NORM : int
+        The maximum gradient norm
+    """
 
     MAX_LENGTH: int = 128
     TRAIN_BATCH_SIZE: int = 4
