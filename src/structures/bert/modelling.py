69bf36c10879d8c7f49ab069e345a00dd3af9c36
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 19:58:40 +0100

    Update src/structures/bert/modelling.py

--- src/structures/bert/modelling.py
+++ src/structures/bert/modelling.py
@@ -1,7 +1,22 @@
 
-import torch.utils.data as tu
+import transformers
+
+import src.structures.bert.parameters
+
 
 class Modelling:
 
-    def __init__(self):
-        pass
+    def __init__(self, device: str, enumerator: dict):
+        """
+
+        :param device: The machine device for computation
+        :param enumerator: The labels enumerator
+        """
+
+        self.__parameters = src.structures.bert.parameters.Parameters()
+
+        # https://huggingface.co/docs/transformers/main_classes/configuration#transformers.PretrainedConfig
+        model = transformers.BertForTokenClassification.from_pretrained(
+            pretrained_model_name_or_path=self.__parameters.pretrained_model_name,
+            **{'num_labels': len(enumerator)})
+        model.to(device)
