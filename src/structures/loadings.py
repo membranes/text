405aeb4500d66875e2d140fb63f76f6cdefdc15c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 18:13:39 +0100

    Update src/structures/loadings.py

--- src/structures/loadings.py
+++ src/structures/loadings.py
@@ -1,10 +1,8 @@
-
-
-import typing
+"""
+Module loadings.py
+"""
 
 import torch.utils.data as tu
-
-import src.elements.variable as va
 
 
 class Loadings:
@@ -16,48 +14,17 @@ class Loadings:
     class for creating DataLoaders.
     """
 
-    def __init__(self, variable: va.Variable,  training: tu.Dataset, validating: tu.Dataset = None,
-                 testing: tu.Dataset = None):
+    def __init__(self):
+        pass
+
+
+    @staticmethod
+    def exc(dataset: tu.Dataset, parameters: dict) -> tu.DataLoader:
         """
 
-        :param variable:
-        :param training:
-        :param validating:
-        :param testing:
-        """
-
-        self.__variable = variable
-
-        self.__training = training
-        self.__validating = validating
-        self.__testing = testing
-
-    def __training_loader(self) -> tu.DataLoader:
-        """
-
+        :param dataset:
+        :param parameters:
         :return:
         """
 
-        parameters = {'batch_size': self.__variable.TRAIN_BATCH_SIZE,
-                      'shuffle': True, 'num_workers': 0}
-
-        return tu.DataLoader(self.__training, **parameters)
-
-    def __validating_loader(self) -> tu.DataLoader:
-        """
-
-        :return:
-        """
-
-        parameters = {'batch_size': self.__variable.VALID_BATCH_SIZE,
-                      'shuffle': True, 'num_workers': 0}
-
-        return tu.DataLoader(self.__validating, **parameters)
-
-    def exc(self) -> typing.Tuple[tu.DataLoader, tu.DataLoader]:
-        """
-
-        :return:
-        """
-
-        return self.__training_loader(), self.__validating_loader()
+        return tu.DataLoader(dataset, **parameters)
