d2fbcc109e483f2f28dcd063282903a7c64e8631
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 01:54:41 +0100

    Update src/structures/initial.py

--- src/structures/initial.py
+++ src/structures/initial.py
@@ -1,30 +1,28 @@
 import logging
+import typing
 
 import pandas as pd
 
-import src.elements.variable
-import src.structures.bert.dataset
-import src.structures.bert.preview
+import config
 
 
 class Initial:
 
-    def __init__(self, frame: pd.DataFrame, enumerator: dict) -> None:
+    def __init__(self, frame: pd.DataFrame) -> None:
 
         self.__frame = frame
-        self.__enumerator = enumerator
-        self.__fraction = 0.8
-        self.__seed = 5
 
-        self.__variable = src.elements.variable.Variable()
-        self.__preview = src.structures.bert.preview.Preview()
+        configurations = config.Config()
+        self.__fraction = configurations.fraction
+        self.__seed = configurations.seed
 
+        # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def __split(self):
+    def __split(self) -> typing.Tuple[pd.DataFrame, pd.DataFrame]:
 
         blob = self.__frame.copy()
 
@@ -34,23 +32,11 @@ class Initial:
         training.reset_index(drop=True, inplace=True)
         validating.reset_index(drop=True, inplace=True)
 
-        return training, validating
-    
-    def __bert(self, blob: pd.DataFrame) -> dict:
-
-        dataset = src.structures.bert.dataset.Dataset(blob, self.__variable, self.__enumerator)
-        self.__logger.info('frame: %s', blob.shape)
-        self.__logger.info('dataset: %s', dataset.__len__())
-
-        self.__preview.exc(dataset=dataset)
-
-        return dataset
-
-    def exc(self):
-
-        training, validating = self.__split()
         self.__logger.info('training: %s', training.shape)
         self.__logger.info('validating: %s', validating.shape)
 
-        self.__bert(blob=training)
-        self.__bert(blob=validating)
+        return training, validating
+
+    def exc(self) -> typing.Tuple[pd.DataFrame, pd.DataFrame]:
+
+        return self.__split()
