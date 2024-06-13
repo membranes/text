41ceb02883a600693f749cd2eb2e13275a12f19c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 21:12:52 +0100

    Update src/models/bert/modelling.py

--- src/models/bert/modelling.py
+++ src/models/bert/modelling.py
@@ -1,23 +1,28 @@
 
 import transformers
 
+import torch
+
 import src.models.bert.parameters
 
 
 class Modelling:
 
-    def __init__(self, device: str, enumerator: dict):
+    def __init__(self, enumerator: dict):
         """
 
-        :param device: The machine device for computation
         :param enumerator: The labels enumerator
         """
 
-        self.__parameters = src.models.bert.parameters.Parameters()
+        # The device for computation
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         # https://huggingface.co/docs/transformers/main_classes/model#transformers.PreTrainedModel.from_pretrained
         # https://huggingface.co/docs/transformers/main_classes/configuration#transformers.PretrainedConfig
+        self.__parameters = src.models.bert.parameters.Parameters()
+
         self.model = transformers.BertForTokenClassification.from_pretrained(
             pretrained_model_name_or_path=self.__parameters.pretrained_model_name,
             **{'num_labels': len(enumerator)})
+
         self.model.to(device)
