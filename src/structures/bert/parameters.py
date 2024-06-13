2379f18f9493f9bb2036f5a9abdc4d208911f2cc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-13 19:58:50 +0100

    Update src/structures/bert/parameters.py

--- src/structures/bert/parameters.py
+++ src/structures/bert/parameters.py
@@ -6,6 +6,9 @@ class Parameters:
 
     def __init__(self):
 
+        # Name
+        self.pretrained_model_name: str = 'google-bert/bert-base-uncased'
+
         # Bases
         self.tokenizer = transformers.BertTokenizerFast.from_pretrained(
-            pretrained_model_name_or_path='google-bert/bert-base-uncased')
+            pretrained_model_name_or_path=self.pretrained_model_name)
