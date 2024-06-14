326428b09e0418996e1b95a85f8f11aeaf6add3d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-14 16:01:41 +0100

    Update src/models/bert/data_collection.py

--- src/models/bert/data_collection.py
+++ src/models/bert/data_collection.py
@@ -7,6 +7,7 @@ import torch.utils.data as tu
 import src.elements.variable
 import src.models.bert.dataset
 import src.models.loadings
+import src.models.bert.preview
 
 
 class DataCollection:
@@ -42,6 +43,7 @@ class DataCollection:
 
         dataset: tu.Dataset = src.models.bert.dataset.Dataset(
             frame=blob, variable=self.__variable, enumerator=self.__enumerator)
+        src.models.bert.preview.Preview().exc(dataset=dataset)
 
         dataloader: tu.DataLoader = self.__loadings.exc(
             dataset=dataset, parameters=parameters)
