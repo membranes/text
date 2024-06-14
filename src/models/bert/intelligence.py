0b2c8041b91faf54c80b7ab9002469230d2f0046
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-14 19:36:52 +0100

    Add src/models/bert/intelligence.py

---
+++ src/models/bert/intelligence.py
@@ -0,0 +1,13 @@
+
+class Intelligence:
+    """
+
+    Cf.
+    https://github.com/huggingface/notebooks/blob/main/examples/token_classification.ipynb
+    https://huggingface.co/docs/transformers/training#training-loop
+    https://colab.research.google.com/github/NielsRogge/Transformers-Tutorials/blob/master/BERT/\
+        Custom_Named_Entity_Recognition_with_BERT_only_first_wordpiece.ipynb#scrollTo=GLFivpkwW1HY
+    """
+
+    def __init__(self):
+        pass
