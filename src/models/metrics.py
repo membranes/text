41fb75bcac9ab0fc01fd6d2af250c27dda647fd5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-15 22:16:28 +0100

    Update src/models/metrics.py

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -1,11 +1,19 @@
+"""Module metrics.py"""
 import logging
 
 import seqeval.metrics as sme
 
 
 class Metrics:
+    """
+    For classification metrics calculations
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
+
         # Logging
         logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
@@ -19,8 +27,14 @@ class Metrics:
         :return:
         """
 
-        self.__logger.info(
-            sme.classification_report(y_true=originals, y_pred=predictions))
+        self.__logger.info(originals)
+        self.__logger.info(predictions)
+
+        y_true = [originals]
+        y_pred = [predictions]
 
         self.__logger.info(
-            sme.accuracy_score(y_true=originals, y_pred=predictions))
+            sme.classification_report(y_true=y_true, y_pred=y_pred))
+
+        self.__logger.info(
+            sme.accuracy_score(y_true=y_true, y_pred=y_pred))
