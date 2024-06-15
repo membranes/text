d310c8762d5bb8ab60eb3e6ea3bbb5c8acaac763
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-15 21:36:18 +0100

    Update src/models/metrics.py

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -11,16 +11,16 @@ class Metrics:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, original: list, predictions: list):
+    def exc(self, originals: list, predictions: list):
         """
 
-        :param original: The true values
+        :param originals: The true values
         :param predictions: The predictions
         :return:
         """
 
         self.__logger.info(
-            sme.classification_report(y_true=original, y_pred=predictions))
+            sme.classification_report(y_true=originals, y_pred=predictions))
 
         self.__logger.info(
-            sme.accuracy_score(y_true=original, y_pred=predictions))
+            sme.accuracy_score(y_true=originals, y_pred=predictions))
