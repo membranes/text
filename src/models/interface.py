5caf99c9b534d3606de019984f44b1d64ea4bb8e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-15 21:31:45 +0100

    Update src/models/interface.py

--- src/models/interface.py
+++ src/models/interface.py
@@ -7,15 +7,17 @@ import src.models.bert.steps
 
 class Interface:
 
-    def __init__(self, frame: pd.DataFrame, enumerator: dict):
+    def __init__(self, frame: pd.DataFrame, enumerator: dict, archetype: dict):
         """
 
         :param frame: The data set for the training, validating, etc., stages
         :param enumerator:
+        :param archetype:
         """
 
         self.__training, self.__validating, _ = src.models.splittings.Splittings(frame=frame).exc()
         self.__enumerator = enumerator
+        self.__archetype = archetype
 
     def exc(self) -> None:
         """
@@ -24,8 +26,8 @@ class Interface:
         """
 
         # bert
-        src.models.bert.steps.Steps(
-            enumerator=self.__enumerator, training=self.__training, validating=self.__validating).exc()
+        src.models.bert.steps.Steps(enumerator=self.__enumerator, archetype=self.__archetype,
+                                    training=self.__training, validating=self.__validating).exc()
 
         # electra
         # src.structures.electra.steps
