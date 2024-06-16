2eaa4f229e05883cebbac8d03bd1f777950c2b43
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-16 02:18:11 +0100

    Add src/elements/collecting.py

---
+++ src/elements/collecting.py
@@ -0,0 +1,20 @@
+import typing
+
+import torch.utils.data as tu
+
+class Collecting(typing.NamedTuple):
+    """
+
+    Attributes
+    ----------
+
+    dataset : torch.utils.data.Dataset
+        A data object for modelling
+
+    dataloader : torch.utils.data.DataLoader
+        A data object that consists of a Dataset, and a
+        set of modelling parameters
+    """
+
+    dataset: tu.Dataset
+    dataloader: tu.DataLoader
