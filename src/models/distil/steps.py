268c7791dc0128ded8b588e464f66b666ac1d1dd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-16 03:39:51 +0100

    Update src/models/distil/steps.py: In progress

--- src/models/distil/steps.py
+++ src/models/distil/steps.py
@@ -1,7 +1,10 @@
 """Module steps.py"""
+import logging
+
 import pandas as pd
 
 import src.elements.variable
+import src.models.structures
 
 
 class Steps:
@@ -22,10 +25,27 @@ class Steps:
         # Inputs
         self.__enumerator = enumerator
         self.__archetype = archetype
-        self.__training = training
-        self.__validating = validating
 
         # A set of values for machine learning model development
         self.__variable = src.elements.variable.Variable()
         self.__variable = self.__variable._replace(EPOCHS=2, TRAIN_BATCH_SIZE=16, VALID_BATCH_SIZE=16)
 
+        # Instances
+        self.__structures = src.models.structures.Structures(
+            enumerator=self.__enumerator, variable=self.__variable,
+            training=training, validating=validating)
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        training = self.__structures.training()
+        validating = self.__structures.validating()
