3c60e450e7972f7ceaccf2bbdcd687c55da9ecf4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-17 18:01:30 +0100

    Update src/models/distil/steps.py

--- src/models/distil/steps.py
+++ src/models/distil/steps.py
@@ -4,6 +4,10 @@ import logging
 import pandas as pd
 
 import src.elements.variable as vr
+import src.elements.frames as fr
+
+import src.models.structures
+import src.models.distil.parameters
 
 
 class Steps:
@@ -11,8 +15,7 @@ class Steps:
     Steps
     """
 
-    def __init__(self, enumerator: dict, archetype: dict,
-                 training: pd.DataFrame, validating: pd.DataFrame):
+    def __init__(self, enumerator: dict, archetype: dict, frames: fr.Frames):
         """
 
         :param enumerator:
@@ -29,6 +32,11 @@ class Steps:
         self.__variable = vr.Variable()
         self.__variable = self.__variable._replace(EPOCHS=2, TRAIN_BATCH_SIZE=16, VALID_BATCH_SIZE=16)
 
+        parameters = src.models.distil.parameters.Parameters()
+        self.__structures = src.models.structures.Structures(
+            enumerator=self.__enumerator, variable=self.__variable,
+            frames=frames, tokenizer=parameters.tokenizer)
+
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -41,5 +49,5 @@ class Steps:
         :return:
         """
 
-        # training = self.__structures.training()
-        # validating = self.__structures.validating()
+        training = self.__structures.training()
+        validating = self.__structures.validating()
