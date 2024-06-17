eabf778fc71aeea03519e56698ddefd612617894
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-17 17:26:50 +0100

    Add src/elements/frames.py

---
+++ src/elements/frames.py
@@ -0,0 +1,24 @@
+import typing
+
+import pandas as pd
+
+
+class Frames(typing.NamedTuple):
+    """
+
+    Attributes
+    ----------
+
+    training : pandas.DataFrame
+        A training data set
+
+    validating : pandas.DataFrame
+        A validation data set
+
+    testing : pandas.DataFrame
+        A testing data set
+    """
+
+    training: pd.DataFrame
+    validating: pd.DataFrame
+    testing: pd.DataFrame = None
