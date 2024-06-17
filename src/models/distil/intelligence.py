31b6840ea1bc29b75d48e86198fa1ac836cf0a44
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-17 13:44:48 +0100

    Update src/models/distil/intelligence.py: In progress

--- src/models/distil/intelligence.py
+++ src/models/distil/intelligence.py
@@ -2,8 +2,8 @@
 import torch.utils.data as tu
 import transformers
 
-import src.elements.variable
-import src.models.distil.parameters
+import src.elements.structures as sr
+import src.elements.variable as vr
 import src.models.distil.parameters
 
 
@@ -12,28 +12,25 @@ class Intelligence:
     Intelligence
     """
 
-    def __init__(self, variable: src.elements.variable.Variable,
-                 enumerator: dict, dataloader: tu.DataLoader):
+    def __init__(self, variable: vr.Variable, enumerator: dict):
         """
 
         :param variable:
         :param enumerator:
-        :param dataloader:
         """
 
         self.__variable = variable
-        self.__dataloader = dataloader
 
         # Parameters
         self.__parameters = src.models.distil.parameters.Parameters()
 
         # https://huggingface.co/docs/transformers/v4.41.3/en/model_doc/auto#transformers.AutoModel
-        model = transformers.AutoModelForTokenClassification.from_pretrained(
+        self.__model = transformers.AutoModelForTokenClassification.from_pretrained(
             pretrained_model_name_or_path=self.__parameters.model_checkpoint,
             **{'num_labels': len(enumerator)})
 
         # https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.TrainingArguments
-        arguments = transformers.TrainingArguments(
+        self.__args = transformers.TrainingArguments(
             output_dir=self.__parameters.model_checkpoint.split('/')[-1],
             evaluation_strategy='epoch',
             learning_rate=self.__variable.LEARNING_RATE,
@@ -42,9 +39,11 @@ class Intelligence:
             num_train_epochs=self.__variable.EPOCHS,
             weight_decay=0.01)
 
-    def __call__(self):
+    def __call__(self, training: sr.Structures, validating: sr.Structures):
         """
         https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.Trainer
 
         :return:
         """
+
+        transformers.Trainer(model=self.__model, args=self.__args, train_dataset=training.dataset, eval_dataset=validating.dataset)
