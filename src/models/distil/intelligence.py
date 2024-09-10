8aef9ec475fd863409ff807f47befb5f2e33dc7b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-10 23:19:28 +0100

    Update src/models/distil/intelligence.py

--- src/models/distil/intelligence.py
+++ src/models/distil/intelligence.py
@@ -1,12 +1,7 @@
 """Module intelligence.py"""
-import logging
-
 import transformers
 
-import src.elements.structures as sr
-import src.elements.variable as vr
 import src.models.distil.parameters as pr
-import src.models.distil.storage
 
 
 class Intelligence:
@@ -14,52 +9,25 @@ class Intelligence:
     Intelligence
     """
 
-    def __init__(self, variable: vr.Variable, enumerator: dict):
+    def __init__(self, enumerator: dict):
         """
 
-        :param variable:
         :param enumerator:
         """
 
-        self.__variable = variable
+        self.__enumerator = enumerator
 
         # Parameters
         self.__parameters = pr.Parameters()
 
-        # Directory preparation
-        src.models.distil.storage.Storage().exc(path=self.__parameters.path)
 
-        # https://huggingface.co/docs/transformers/v4.41.3/en/model_doc/auto#transformers.AutoModel
-        self.__model = transformers.AutoModelForTokenClassification.from_pretrained(
-            pretrained_model_name_or_path=self.__parameters.pretrained_model_name,
-            **{'num_labels': len(enumerator)})
-        self.__model.to(self.__parameters.device)
-
-    def __args(self):
-
-        # https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.TrainingArguments
-        return transformers.TrainingArguments(
-            output_dir=self.__parameters.path,
-            eval_strategy='epoch',
-            learning_rate=self.__variable.LEARNING_RATE,
-            per_device_train_batch_size=self.__variable.TRAIN_BATCH_SIZE,
-            per_device_eval_batch_size=self.__variable.VALID_BATCH_SIZE,
-            num_train_epochs=self.__variable.EPOCHS,
-            weight_decay=0.01)
-
-
-    def __call__(self, training: sr.Structures, validating: sr.Structures,
-                 tokenizer: transformers.PreTrainedTokenizerBase) -> transformers.Trainer:
+    def model(self):
         """
-        https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.Trainer
+        https://huggingface.co/docs/transformers/v4.41.3/en/model_doc/auto#transformers.AutoModel
 
         :return:
         """
 
-        trainer = transformers.Trainer(
-            model=self.__model, args=self.__args(), train_dataset=training.dataset,
-            eval_dataset=validating.dataset, tokenizer=tokenizer)
-
-        trainer.train()
-
-        return trainer
+        return transformers.AutoModelForTokenClassification.from_pretrained(
+            pretrained_model_name_or_path=self.__parameters.pretrained_model_name,
+            **{'num_labels': len(self.__enumerator)})
