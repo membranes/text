d2d869d97c662f326160f49175bebe775d2e2e83
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-10 19:13:04 +0100

    Update src/models/distil/intelligence.py

--- src/models/distil/intelligence.py
+++ src/models/distil/intelligence.py
@@ -5,7 +5,8 @@ import transformers
 
 import src.elements.structures as sr
 import src.elements.variable as vr
-import src.models.distil.parameters
+import src.models.distil.parameters as pr
+import src.models.distil.storage
 
 
 class Intelligence:
@@ -23,7 +24,10 @@ class Intelligence:
         self.__variable = variable
 
         # Parameters
-        self.__parameters = src.models.distil.parameters.Parameters()
+        self.__parameters = pr.Parameters()
+
+        # Directory preparation
+        src.models.distil.storage.Storage().exc(path=self.__parameters.path)
 
         # https://huggingface.co/docs/transformers/v4.41.3/en/model_doc/auto#transformers.AutoModel
         self.__model = transformers.AutoModelForTokenClassification.from_pretrained(
@@ -31,9 +35,11 @@ class Intelligence:
             **{'num_labels': len(enumerator)})
         self.__model.to(self.__parameters.device)
 
+    def __args(self):
+
         # https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.TrainingArguments
-        self.__args = transformers.TrainingArguments(
-            output_dir=self.__parameters.pretrained_model_name.split('/')[-1],
+        return transformers.TrainingArguments(
+            output_dir=self.__parameters.path,
             eval_strategy='epoch',
             learning_rate=self.__variable.LEARNING_RATE,
             per_device_train_batch_size=self.__variable.TRAIN_BATCH_SIZE,
@@ -41,13 +47,9 @@ class Intelligence:
             num_train_epochs=self.__variable.EPOCHS,
             weight_decay=0.01)
 
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
-    def __call__(self, training: sr.Structures, validating: sr.Structures) -> transformers.Trainer:
+    def __call__(self, training: sr.Structures, validating: sr.Structures,
+                 tokenizer: transformers.PreTrainedTokenizerBase) -> transformers.Trainer:
         """
         https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.Trainer
 
@@ -55,8 +57,8 @@ class Intelligence:
         """
 
         trainer = transformers.Trainer(
-            model=self.__model, args=self.__args, train_dataset=training.dataset,
-            eval_dataset=validating.dataset, tokenizer=self.__parameters.tokenizer)
+            model=self.__model, args=self.__args(), train_dataset=training.dataset,
+            eval_dataset=validating.dataset, tokenizer=tokenizer)
 
         trainer.train()
 
