d8d3046100f0c3ac54c0eb6ed37bc11cdcf0f090
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-10 15:48:43 +0100

    Update src/data/source.py

--- src/data/source.py
+++ src/data/source.py
@@ -15,6 +15,11 @@ class Source:
         self.__names: dict[str, str] = {'Word': 'word', 'POS': 'part', 'Tag': 'tag'}
 
     def __read(self) -> dfr.DataFrame:
+        """
+        Reads-in the modelling data set.
+
+        :return:
+        """
 
         try:
             frame: dfr.DataFrame = dfr.read_csv(path=os.path.join(self.__datapath, 'dataset.csv'), header=0)
@@ -27,6 +32,12 @@ class Source:
         return frame
 
     def __rename(self, blob: dfr.DataFrame) -> dfr.DataFrame:
+        """
+        Renames the fields
+
+        :param blob: A dask data frame
+        :return:
+        """
 
         frame: dfr.DataFrame = blob.copy()
         frame: dfr.DataFrame = frame.rename(columns=self.__names)
@@ -35,6 +46,12 @@ class Source:
     
     @staticmethod
     def __tag_splits(blob: dfr.DataFrame) -> dfr.DataFrame:
+        """
+        Splits each tag into its annotation & category parts.
+
+        :param blob:
+        :return:
+        """
 
         splits: dfr.DataFrame = blob['tag'].str.split(pat='-', n=2, expand=True)
         splits: dfr.DataFrame = splits.rename(columns={0: 'annotation', 1: 'category'})
@@ -44,6 +61,10 @@ class Source:
         return frame
 
     def exc(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
 
         frame: dfr.DataFrame = self.__read()
         frame: dfr.DataFrame = self.__rename(blob=frame)
