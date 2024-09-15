eda3da776db083ac506cf26d0635c1ba45607e6c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-15 02:55:39 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -4,14 +4,25 @@ import sys
 
 import pandas as pd
 import torch
+import ray
+
 
 def main():
+    """
+    https://docs.ray.io/en/latest/ray-core/api/doc/ray.init.html
+    https://docs.ray.io/en/latest/ray-core/configure.html
+
+    :return:
+    """
 
     logger: logging.Logger = logging.getLogger(__name__)
 
     # Device Selection: Setting a graphics processing unit as the default device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     logger.info(msg=device)
+
+    # Ray
+    ray.init(dashboard_host='172.17.0.2', dashboard_port=8265)
 
     # The Data
     data: pd.DataFrame = src.data.source.Source().exc()
@@ -49,6 +60,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Activate graphics processing units
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
+    os.environ['TOKENIZERS_PARALLELISM']='true'
+    os.environ['RAY_USAGE_STATS_ENABLED']='0'
     
     # Modules
     import src.data.source
@@ -56,5 +72,5 @@ if __name__ == '__main__':
     import src.data.demarcations
     import src.functions.cache
     import src.models.interface
-    
+
     main()
