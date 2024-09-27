1649e33f77b42ab9b0d4ffdbf48fff689f64a87e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-27 14:22:03 +0100

    Update src/models/distil/dataset.py: In Progress

--- src/models/distil/dataset.py
+++ src/models/distil/dataset.py
@@ -1,3 +1,4 @@
+import logging
 import torch.utils.data
 import transformers
 
@@ -17,11 +18,18 @@ class Dataset(torch.utils.data.Dataset):
         self.__frame = frame
         self.__tokenizer = tokenizer
 
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
     def __getitem__(self, index):
 
         words: list[str] = self.__frame['sentence'][index].strip().split()
         encodings = self.__tokenizer(words, truncation=True, is_split_into_words=True)
         tags: list[str] = self.__frame['tagstr'][index].split(',')
 
-
-
+        self.__logger.info(words)
+        self.__logger.info(encodings)
+        self.__logger.info(tags)
