a359451e18e3e5208db23942fe0d42a08532655f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-27 14:23:57 +0100

    Update src/models/distil/structures.py: In Progress

--- src/models/distil/structures.py
+++ src/models/distil/structures.py
@@ -1,5 +1,24 @@
 
+import transformers
+
+import src.elements.variable as vr
+import src.elements.frames as fr
+
+
 class Structures:
 
-    def __init__(self):
-        pass
+    def __init__(self, enumerator: dict, variable: vr.Variable, frames: fr.Frames,
+                 tokenizer: transformers.tokenization_utils_base):
+        """
+
+        :param enumerator:
+        :param variable:
+        :param frames:
+        """
+
+        # A set of values, and data, for machine learning model development
+        self.__enumerator = enumerator
+        self.__variable = variable
+        self.__frames = frames
+
+        self.__tokenizer = tokenizer
