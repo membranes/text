4cb95183a56abb26d1bc48ee22fb149230fe060c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-27 13:54:56 +0100

    Update src/models/distil/dataset.py

---
+++ src/models/distil/dataset.py
@@ -0,0 +1,26 @@
+import torch.utils.data
+import transformers
+
+import pandas as pd
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, frame: pd.DataFrame, tokenizer: transformers.tokenization_utils_base):
+        """
+
+        :param frame:
+        :param tokenizer:
+        """
+
+        super().__init__()
+
+        self.__frame = frame
+        self.__tokenizer = tokenizer
+
+    def __getitem__(self, index):
+
+        words: list[str] = self.__frame['sentence'][index].strip().split()
+        encodings = self.__tokenizer(words, truncation=True, is_split_into_words=True)
+
+
+
