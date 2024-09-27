8305901483bfc94da5c6a178917567c5c53c6d92
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-27 14:52:05 +0100

    Update src/models/distil/structures.py: In Progress

--- src/models/distil/structures.py
+++ src/models/distil/structures.py
@@ -1,9 +1,12 @@
-
+import logging
+import pandas as pd
 import transformers
 
 import src.elements.frames as fr
 import src.elements.variable as vr
+import src.elements.structures as sr
 import src.models.loadings
+import src.models.distil.dataset
 
 
 class Structures:
@@ -26,3 +29,55 @@ class Structures:
 
         # For DataLoader creation
         self.__loadings = src.models.loadings.Loadings()
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def __structure(self, frame: pd.DataFrame, parameters: dict):
+
+
+        self.__logger.info(parameters)
+
+        src.models.distil.dataset.Dataset(frame=frame, tokenizer=self.__tokenizer)
+
+    def training(self):
+        """
+        Delivers the training data's Dataset & DataLoader
+
+        :return:
+        """
+
+        # Modelling parameters
+        parameters = {'batch_size': self.__variable.TRAIN_BATCH_SIZE,
+                      'shuffle': True, 'num_workers': 0}
+
+        return self.__structure(frame=self.__frames.training, parameters=parameters)
+
+    def validating(self):
+        """
+        Delivers the validation data's Dataset & DataLoader
+
+        :return:
+        """
+
+        # Modelling parameters
+        parameters = {'batch_size': self.__variable.VALID_BATCH_SIZE,
+                      'shuffle': True, 'num_workers': 0}
+
+        return self.__structure(frame=self.__frames.validating, parameters=parameters)
+
+    def testing(self):
+        """
+        Delivers the testing data's Dataset & DataLoader
+
+        :return:
+        """
+
+        # Modelling parameters
+        parameters = {'batch_size': self.__variable.TEST_BATCH_SIZE,
+                      'shuffle': True, 'num_workers': 0}
+
+        return self.__structure(frame=self.__frames.testing, parameters=parameters)
