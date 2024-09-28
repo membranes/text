64640a478194038bb9daaad845f065f7311fbe35
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-28 19:35:11 +0100

    Update src/models/distil/settings.py

--- src/models/distil/settings.py
+++ src/models/distil/settings.py
@@ -27,16 +27,17 @@ class Settings:
     @staticmethod
     def hp_space(trial):
         """
-        'learning_rate': self.__variable.LEARNING_RATE,
-        'weight_decay': self.__variable.WEIGHT_DECAY
+        If the search algorithm depends on a continuous distribution, e.g., BayesOpt,
+        do not use a categorical sample space, e.g., ray.tune.choice
+
+        'per_device_train_batch_size': ray.tune.choice([4, 16, 32]),
+        'num_train_epochs': ray.tune.choice([2, 4])
 
         :return:
         """
 
-        return {'learning_rate': ray.tune.uniform(lower=0.001, upper=0.1),
-                'weight_decay': ray.tune.uniform(lower=0.01, upper=0.1),
-                'per_device_train_batch_size': ray.tune.choice([4, 16, 32]),
-                'num_train_epochs': ray.tune.choice([2, 4])}
+        return {'learning_rate': ray.tune.quniform(lower=0.001, upper=0.002, q=0.001),
+                'weight_decay': ray.tune.quniform(lower=0.01, upper=0.02, q=0.01)}
 
     def scheduler(self):
         """
