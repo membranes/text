eaf1db31ecd0a44bc7c19fd8f877faaafd161380
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-30 15:10:05 +0100

    Update src/models/distil/architecture.py

--- src/models/distil/architecture.py
+++ src/models/distil/architecture.py
@@ -120,6 +120,6 @@ class Architecture:
 
             # run configuration: local_dir -> storage_path
             name='default', storage_path=os.path.join(self.__parameters.storage_path, 'ray'),
-            verbose=0, progress_reporter=settings.reporting(), log_to_file=True)
+            verbose=0, progress_reporter=settings.reporting, log_to_file=True)
 
         return best
