2ad7efe56277188e72a25a7e50ebf6052ec9c36a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-07 19:49:16 +0100

    Add src/models/bert/measurements.py

---
+++ src/models/bert/measurements.py
@@ -0,0 +1,37 @@
+
+import logging
+
+import seqeval.metrics as sme
+
+
+class Measurements:
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        # Logging
+        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self, originals: list, predictions: list):
+        """
+
+        :param originals: The true values
+        :param predictions: The predictions
+        :return:
+        """
+
+        self.__logger.info(originals)
+        self.__logger.info(predictions)
+
+        y_true = [originals]
+        y_pred = [predictions]
+
+        self.__logger.info(
+            sme.classification_report(y_true=y_true, y_pred=y_pred, zero_division=0.0))
+
+        self.__logger.info(
+            sme.accuracy_score(y_true=y_true, y_pred=y_pred))
