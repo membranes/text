7a7db0d5ec46a620feb625e50374925d29d8a8ad
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-15 17:01:24 +0100

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def main():
     # Temporary
     data = data.loc[:500, :]
     src.models.interface.Interface(
-        data=data, enumerator=interface.enumerator(), archetype=interface.archetype()).exc()
+        data=data, enumerator=interface.enumerator(), archetype=interface.archetype()).exc(architecture='bert')
 
     # Delete Cache Points
     src.functions.cache.Cache().exc()
