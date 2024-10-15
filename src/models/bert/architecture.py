d7ddc6d9693143a07f3234cb699f2b727584fa0d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-15 10:15:02 +0100

    Update src/models/bert/architecture.py: Temporary

--- src/models/bert/architecture.py
+++ src/models/bert/architecture.py
@@ -1,5 +1,5 @@
 """Module architecture.py"""
-
+import os
 import transformers
 
 import src.elements.structures as sr
@@ -36,7 +36,7 @@ class Architecture:
         src.functions.directories.Directories().cleanup(path=self.__parameters.storage_path)
 
     def __call__(self, training: sr.Structures, validating: sr.Structures,
-                 tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase):
+                 tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase) -> transformers.trainer_utils.BestRun:
         """
         https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.Trainer
         https://docs.ray.io/en/latest/tune/api/doc/ray.tune.run.html
@@ -58,3 +58,36 @@ class Architecture:
 
         # Settings
         settings = src.models.bert.settings.Settings(variable=self.__variable)
+
+        # Hence
+        trainer = transformers.Trainer(
+            model_init=intelligence.model,
+            args=args, data_collator=intelligence.collator(tokenizer=tokenizer),
+            train_dataset=training.dataset, eval_dataset=validating.dataset,
+            tokenizer=tokenizer,
+            compute_metrics=metrics.exc
+        )
+
+        best = trainer.hyperparameter_search(
+            hp_space=settings.hp_space,
+            compute_objective=settings.compute_objective,
+            n_trials=self.__variable.N_TRIALS,
+            direction='minimize',
+            backend='ray',
+
+            # scaling configuration
+            resources_per_trial={'cpu': self.__variable.N_CPU, 'gpu': self.__variable.N_GPU},
+
+            # tune configuration
+            search_alg=settings.algorithm(),
+            scheduler=settings.scheduler(), reuse_actors=True,
+
+            # check point configuration
+            # keep_checkpoints_num=8, checkpoint_score_attr='training_iteration',
+
+            # run configuration: local_dir -> storage_path
+            name='default', storage_path=os.path.join(self.__parameters.storage_path, 'ray'),
+            verbose=0, progress_reporter=settings.reporting, log_to_file=True
+        )
+
+        return best
