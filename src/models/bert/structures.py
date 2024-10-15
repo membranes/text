b1a9ee829e61c500f74d2a23f98b3f69d065ce37
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-15 12:14:43 +0100

    Update src/models/bert/structures.py

--- src/models/bert/structures.py
+++ src/models/bert/structures.py
@@ -1,5 +1,4 @@
 """Module structures.py"""
-import logging
 
 import pandas as pd
 import torch.utils.data as tu
@@ -38,12 +37,6 @@ class Structures:
 
         # For DataLoader creation
         self.__loadings = src.models.loadings.Loadings()
-
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
     def __structure(self, frame: pd.DataFrame, parameters: dict) -> sr.Structures:
         """
