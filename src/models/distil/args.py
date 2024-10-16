e226cb401fed034a06fe20a25258d9967a07ea2c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-16 19:31:39 +0100

    Update src/models/distil/args.py

--- src/models/distil/args.py
+++ src/models/distil/args.py
@@ -4,8 +4,7 @@ import os
 import transformers
 
 import config
-import src.elements.variable as vr
-import src.models.distil.parameters
+import src.elements.arguments as ag
 
 
 class Args:
@@ -13,36 +12,39 @@ class Args:
     Class Arguments
     """
 
-    def __init__(self, variable: vr.Variable):
+    def __init__(self, arguments: ag.Arguments):
         """
 
-        :param variable:
+        :param arguments:
         """
 
-        self.__variable = variable
-
-        self.__parameters = src.models.distil.parameters.Parameters()
+        self.__arguments = arguments
 
     def exc(self):
         """
-        https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.TrainingArguments
 
-        TensorBoard logging directory: output_dir/runs/CURRENT_DATETIME_HOSTNAME*
-            https://huggingface.co/docs/transformers/v4.44.2/en/main_classes/
-                &amp;num;transformers.TrainingArguments.logging_dir
+        Notes<br>
+        -----<br>
+        output_dir: model_output_directory<br>
+        logging_dir: storage_path, <a href="https://huggingface.co/docs/transformers/v4.45.2/en/main_classes/trainer#transformers.TrainingArguments.logging_dir" target="_blank">
+        TensorBoard logging directory</a><br><br>
+
+        References<br>
+        ----------<br>
+        <a href="https://huggingface.co/docs/transformers/v4.41.3/en/main_classes/trainer#transformers.TrainingArguments" target="_blank">TrainingArguments</a>
 
         :return:
         """
 
         return transformers.TrainingArguments(
-            output_dir=self.__parameters.MODEL_OUTPUT_DIRECTORY,
+            output_dir=self.__arguments.model_output_directory,
             eval_strategy='epoch',
             save_strategy='epoch',
-            learning_rate=self.__variable.LEARNING_RATE,
-            weight_decay=self.__variable.WEIGHT_DECAY,
-            per_device_train_batch_size=self.__variable.TRAIN_BATCH_SIZE,
-            per_device_eval_batch_size=self.__variable.VALID_BATCH_SIZE,
-            num_train_epochs=self.__variable.EPOCHS,
+            learning_rate=self.__arguments.LEARNING_RATE,
+            weight_decay=self.__arguments.WEIGHT_DECAY,
+            per_device_train_batch_size=self.__arguments.TRAIN_BATCH_SIZE,
+            per_device_eval_batch_size=self.__arguments.VALID_BATCH_SIZE,
+            num_train_epochs=self.__arguments.EPOCHS,
             max_steps=-1,
             warmup_steps=0,
             no_cuda=False,
@@ -50,6 +52,6 @@ class Args:
             save_total_limit=5,
             skip_memory_metrics=True,
             load_best_model_at_end=True,
-            logging_dir=os.path.join(self.__parameters.MODEL_OUTPUT_DIRECTORY, 'logs'),
+            logging_dir=os.path.join(self.__arguments.model_output_directory, 'logs'),
             fp16=True,
             push_to_hub=False)
