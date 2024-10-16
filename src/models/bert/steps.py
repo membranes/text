c0d380db6e289aac79fa209a9280bdc052af5643
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-17 00:21:56 +0100

    Delete src/models/bert/steps.py

--- src/models/bert/steps.py
+++ src/models/bert/steps.py
@@ -7,11 +7,10 @@ import src.elements.arguments as ag
 import src.elements.frames as fr
 import src.models.bert.architecture
 import src.models.bert.operating
-import src.models.bert.parameters
+import src.models.bert.structures
 import src.models.bert.tokenizer
 import src.models.bert.validation
 import src.models.measurements
-import src.models.bert.structures
 
 
 class Steps:
