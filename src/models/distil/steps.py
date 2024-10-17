b7fe96da132e3fa0354d2880828e11e8d75a49cc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-17 18:31:47 +0100

    Update src/models/distil/steps.py

--- src/models/distil/steps.py
+++ src/models/distil/steps.py
@@ -4,13 +4,13 @@ import logging
 import transformers.tokenization_utils_base
 
 import src.elements.arguments as ag
-import src.elements.frames as fr
-import src.models.optimal
-import src.models.operating
+import src.elements.vault as vu
 import src.models.distil.structures
 import src.models.distil.tokenizer
-import src.models.validation
 import src.models.measurements
+import src.models.operating
+import src.models.optimal
+import src.models.validation
 
 
 class Steps:
@@ -19,24 +19,24 @@ class Steps:
     ref. https://huggingface.co/docs/transformers/tasks/token_classification
     """
 
-    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, frames: fr.Frames):
+    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, vault: vu.Vault):
         """
 
         :param enumerator: The tags and their identification codes.
         :param archetype: The inverse dict of enumerator.
         :param arguments: The parameter values for ...
-        :param frames: An object of dataframes, consisting of the training, validating, and testing data sets.
+        :param vault: An object of dataframes, consisting of the training, validating, and testing data sets.
         """
 
         # Inputs
         self.__enumerator = enumerator
         self.__archetype = archetype
         self.__arguments = arguments
-        self.__frames = frames
+        self.__vault = vault
 
         # A set of values for machine learning model development
         self.__arguments = self.__arguments._replace(
-            N_TRAIN=self.__frames.training.shape[0], N_VALID=self.__frames.validating.shape[0], N_TEST=self.__frames.testing.shape[0])
+            N_TRAIN=self.__vault.training.shape[0], N_VALID=self.__vault.validating.shape[0], N_TEST=self.__vault.testing.shape[0])
 
         # Get tokenizer
         self.__tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase = (
@@ -56,7 +56,7 @@ class Steps:
 
         structures = src.models.distil.structures.Structures(
             enumerator=self.__enumerator, arguments=self.__arguments,
-            frames=self.__frames, tokenizer=self.__tokenizer)
+            vault=self.__vault, tokenizer=self.__tokenizer)
 
         return structures.training(), structures.validating(), structures.testing()
 
