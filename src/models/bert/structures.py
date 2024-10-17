1f59aa0dd44f1f1470d319ddec55192e6f339c27
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-17 18:31:58 +0100

    Upodate src/models/bert/structures.py

--- src/models/bert/structures.py
+++ src/models/bert/structures.py
@@ -1,12 +1,11 @@
 """Module structures.py"""
-
 import pandas as pd
 import torch.utils.data as tu
 import transformers
 
 import src.elements.arguments as ag
-import src.elements.frames as fr
 import src.elements.structures as sr
+import src.elements.vault as vu
 import src.models.bert.dataset
 import src.models.loader
 
@@ -19,20 +18,20 @@ class Structures:
     Builds and delivers the data structures per modelling stage
     """
 
-    def __init__(self, enumerator: dict, arguments: ag.Arguments, frames: fr.Frames,
+    def __init__(self, enumerator: dict, arguments: ag.Arguments, vault: vu.Vault,
                  tokenizer: transformers.tokenization_utils_base):
         """
 
         :param enumerator:
         :param arguments:
-        :param frames:
+        :param vault:
         :param tokenizer:
         """
 
         # A set of values, and data, for machine learning model development
         self.__enumerator = enumerator
         self.__arguments = arguments
-        self.__frames = frames
+        self.__vault = vault
 
         self.__tokenizer = tokenizer
 
@@ -69,7 +68,7 @@ class Structures:
         parameters = {'batch_size': self.__arguments.TRAIN_BATCH_SIZE,
                       'shuffle': True, 'num_workers': 0}
 
-        return self.__structure(frame=self.__frames.training, parameters=parameters)
+        return self.__structure(frame=self.__vault.training, parameters=parameters)
 
     def validating(self) -> sr.Structures:
         """
@@ -82,7 +81,7 @@ class Structures:
         parameters = {'batch_size': self.__arguments.VALID_BATCH_SIZE,
                       'shuffle': True, 'num_workers': 0}
 
-        return self.__structure(frame=self.__frames.validating, parameters=parameters)
+        return self.__structure(frame=self.__vault.validating, parameters=parameters)
 
     def testing(self) -> sr.Structures:
         """
@@ -95,4 +94,4 @@ class Structures:
         parameters = {'batch_size': self.__arguments.TEST_BATCH_SIZE,
                       'shuffle': True, 'num_workers': 0}
 
-        return self.__structure(frame=self.__frames.testing, parameters=parameters)
+        return self.__structure(frame=self.__vault.testing, parameters=parameters)
