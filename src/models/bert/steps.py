10f190469b6597aff0bd4c5dd5c7b504075b3fbf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-17 18:31:35 +0100

    Update src/models/bert/steps.py

--- src/models/bert/steps.py
+++ src/models/bert/steps.py
@@ -4,13 +4,13 @@ import logging
 import transformers
 
 import src.elements.arguments as ag
-import src.elements.frames as fr
-import src.models.optimal
-import src.models.operating
+import src.elements.vault as vu
 import src.models.bert.structures
 import src.models.bert.tokenizer
-import src.models.validation
 import src.models.measurements
+import src.models.operating
+import src.models.optimal
+import src.models.validation
 
 
 class Steps:
@@ -18,13 +18,13 @@ class Steps:
     The BERT steps.
     """
 
-    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, frames: fr.Frames):
+    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, vault: vu.Vault):
         """
 
         :param enumerator: Code -> tag mapping
         :param archetype: Tag -> code mapping
         :param arguments: The parameter values for ...
-        :param frames: The data frames for modelling stages, i.e., the
+        :param vault: The data frames for modelling stages, i.e., the
                        training, validating, and testing stages
         """
 
@@ -32,11 +32,11 @@ class Steps:
         self.__enumerator = enumerator
         self.__archetype = archetype
         self.__arguments = arguments
-        self.__frames = frames
+        self.__vault = vault
 
         # A set of values for machine learning model development
         self.__arguments = self.__arguments._replace(
-            N_TRAIN=self.__frames.training.shape[0], N_VALID=self.__frames.validating.shape[0], N_TEST=self.__frames.testing.shape[0])
+            N_TRAIN=self.__vault.training.shape[0], N_VALID=self.__vault.validating.shape[0], N_TEST=self.__vault.testing.shape[0])
 
         # Get tokenizer
         self.__tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase = (
@@ -50,7 +50,7 @@ class Steps:
 
         structures = src.models.bert.structures.Structures(
             enumerator=self.__enumerator, arguments=self.__arguments,
-            frames=self.__frames, tokenizer=self.__tokenizer)
+            vault=self.__vault, tokenizer=self.__tokenizer)
 
         return structures.training(), structures.validating(), structures.testing()
 
