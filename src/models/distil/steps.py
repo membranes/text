2ea0069347d42bf0889efaf0438327442daba1c8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-18 20:34:07 +0100

    Update src/models/distil/steps.py

--- src/models/distil/steps.py
+++ src/models/distil/steps.py
@@ -4,6 +4,7 @@ import logging
 import transformers.tokenization_utils_base
 
 import src.elements.arguments as ag
+import src.elements.hyperspace as hp
 import src.elements.vault as vu
 import src.models.distil.structures
 import src.models.distil.tokenizer
@@ -19,12 +20,13 @@ class Steps:
     ref. https://huggingface.co/docs/transformers/tasks/token_classification
     """
 
-    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, vault: vu.Vault):
+    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments, hyperspace: hp.Hyperspace, vault: vu.Vault):
         """
 
         :param enumerator: The tags and their identification codes.
         :param archetype: The inverse dict of enumerator.
         :param arguments: The parameter values for ...
+        :param hyperspace: The real number spaces of ...
         :param vault: An object of dataframes, consisting of the training, validating, and testing data sets.
         """
 
@@ -32,6 +34,7 @@ class Steps:
         self.__enumerator = enumerator
         self.__archetype = archetype
         self.__arguments = arguments
+        self.__hyperspace = hyperspace
         self.__vault = vault
 
         # A set of values for machine learning model development
@@ -41,12 +44,6 @@ class Steps:
         # Get tokenizer
         self.__tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase = (
             src.models.distil.tokenizer.Tokenizer(arguments=self.__arguments)())
-
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
     def __structures(self):
         """
@@ -67,25 +64,24 @@ class Steps:
         """
 
         training, validating, _ = self.__structures()
-        self.__logger.info(self.__arguments)
 
         # Hyperparameter search
         optimal = src.models.optimal.Optimal(
-            arguments=self.__arguments, enumerator=self.__enumerator, archetype=self.__archetype)
+            arguments=self.__arguments, hyperspace=self.__hyperspace,
+            enumerator=self.__enumerator, archetype=self.__archetype)
         best = optimal(training=training, validating=validating, tokenizer=self.__tokenizer)
-        self.__logger.info(best)
+        logging.info(best)
 
         # Hence, update the modelling variables
         self.__arguments = self.__arguments._replace(
             LEARNING_RATE=best.hyperparameters.get('learning_rate'), WEIGHT_DECAY=best.hyperparameters.get('weight_decay'),
             TRAIN_BATCH_SIZE=best.hyperparameters.get('per_device_train_batch_size'))
-        self.__logger.info(self.__arguments)
+        logging.info(self.__arguments)
 
         # Training via the best hyperparameters set
         operating = src.models.operating.Operating(
             arguments=self.__arguments, enumerator=self.__enumerator, archetype=self.__archetype)
         model = operating.exc(training=training, validating=validating, tokenizer=self.__tokenizer)
-        self.__logger.info(dir(model))
 
         # Evaluating: vis-à-vis model & validation data
         originals, predictions = src.models.validation.Validation(
