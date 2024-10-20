f76fe36cc5abd62c920b1c1a9e14be2bab9b38c9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-20 22:16:52 +0100

    Update src/models/operating.py

--- src/models/operating.py
+++ src/models/operating.py
@@ -26,7 +26,7 @@ class Operating:
         self.__archetype = archetype
 
         # Intelligence
-        self.__algorithm = src.models.algorithm.Algorithm(architecture=self.__arguments.name)
+        self.__algorithm = src.models.algorithm.Algorithm(architecture=self.__arguments.architecture)
 
     def exc(self, training: sr.Structures, validating: sr.Structures,
             tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase):
