ce505d3e0433acdbf2d926a2d8f1cc971419cc05
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-29 20:24:06 +0000

    Update src/models/numerics.py

--- src/models/numerics.py
+++ src/models/numerics.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import sklearn.metrics as sm
 
+import dask
+
 
 class Numerics:
 
@@ -19,3 +21,13 @@ class Numerics:
             y_true=_true, y_pred=_prediction).ravel()
 
         return {name: {'tn': tn, 'fp': fp, 'fn': fn, 'tp': tp}}
+
+    def exc(self):
+
+        names = np.unique(self.__originals)
+        objects = [dask.delayed(self.__measures)(name) for name in names]
+        calculations = dask.compute(objects)[0]
+
+        structure = {k: v for c in calculations for k, v in c.items()}
+
+        return structure
