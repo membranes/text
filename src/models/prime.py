6c88683d61f3a53be3914f8e622c8f9539f4a97b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-20 12:01:40 +0000

    Update src/models/prime.py

--- src/models/prime.py
+++ src/models/prime.py
@@ -1,13 +1,12 @@
 """Module prime.py"""
-import os
-
-import transformers
 import datasets
+import transformers
 
 import src.elements.arguments as ag
-import src.models.measurements
-import src.models.recompute
-import src.models.validation
+import src.models.algorithm
+import src.models.metrics
+import src.models.training_arguments
+import src.models.prerequisites
 
 
 class Prime:
@@ -18,38 +17,32 @@ class Prime:
     Determines the prime model vis-à-vis the best set of hyperparameters.
     """
 
-    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments,
-                 tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase):
+    def __init__(self, enumerator: dict, archetype: dict, arguments: ag.Arguments):
         """
 
         :param enumerator: Of tags; key &rarr; identifier, value &rarr; label<br>
         :param archetype: Of tags; key &rarr; label, value &rarr; identifier<br>
         :param arguments: A suite of values/arguments for machine learning model development.<br>
-        :param tokenizer: The tokenizer of text.<br>
         """
 
         self.__enumerator = enumerator
         self.__archetype = archetype
         self.__arguments = arguments
-        self.__tokenizer = tokenizer
 
-    def exc(self, training: datasets.Dataset, validating: datasets.Dataset):
+    def exc(self, training: datasets.Dataset, validating: datasets.Dataset,
+            tokenizer: transformers.tokenization_utils_base.PreTrainedTokenizerBase):
         """
 
         :param training: The training data.
         :param validating: The validation data.
+        :param tokenizer: The tokenizer of text.<br>
         :return:
         """
 
-        # Training via the best hyperparameters set
-        recompute = src.models.recompute.Recompute(
-            arguments=self.__arguments, enumerator=self.__enumerator, archetype=self.__archetype)
-        model = recompute.exc(training=training, validating=validating, tokenizer=self.__tokenizer)
-        model.save_model(output_dir=os.path.join(self.__arguments.model_output_directory, 'model'))
+        trainer = src.models.prerequisites.Prerequisites(
+            arguments=self.__arguments, enumerator=self.__enumerator, archetype=self.__archetype)(
+            training=training, validating=validating, tokenizer=tokenizer)
 
-        # Evaluating: vis-à-vis model & validation data
-        originals, predictions = src.models.validation.Validation(
-            validating=validating, archetype=self.__archetype).exc(model=model)
+        trainer.train()
 
-        src.models.measurements.Measurements(
-            originals=originals, predictions=predictions, arguments=self.__arguments).exc(segment='prime')
+        return trainer
