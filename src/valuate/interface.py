03b8a72edadba2d9e83a3d94d41a8e592d77499f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-22 21:57:12 +0000

    Update src/valuate/interface.py

--- src/valuate/interface.py
+++ src/valuate/interface.py
@@ -12,17 +12,15 @@ class Interface:
     Interface
     """
 
-    def __init__(self, model: transformers.Trainer, archetype: dict, arguments: ag.Arguments,):
+    def __init__(self, model: transformers.Trainer, archetype: dict):
         """
 
         :param model:
         :param archetype:
-        :param arguments:
         """
 
         self.__model = model
         self.__archetype = archetype
-        self.__arguments = arguments
 
     def exc(self, blob: datasets.Dataset, path: str):
         """
@@ -36,4 +34,4 @@ class Interface:
             blob=blob, archetype=self.__archetype).exc(model=self.__model)
 
         src.valuate.measurements.Measurements(
-            originals=originals, predictions=predictions, arguments=self.__arguments).exc(path=path)
+            originals=originals, predictions=predictions).exc(path=path)
