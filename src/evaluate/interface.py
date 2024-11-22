d7692caf2410689a176fc19a146f45040cdc0e4f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-22 19:54:17 +0000

    Update src/evaluate/interface.py

--- src/evaluate/interface.py
+++ src/evaluate/interface.py
@@ -12,17 +12,19 @@ class Interface:
     Interface
     """
 
-    def __init__(self, model: transformers.Trainer, archetype: dict):
+    def __init__(self, model: transformers.Trainer, archetype: dict, arguments: ag.Arguments,):
         """
 
         :param model:
         :param archetype:
+        :param arguments:
         """
 
         self.__model = model
         self.__archetype = archetype
+        self.__arguments = arguments
 
-    def exc(self, blob: datasets.Dataset, arguments: ag.Arguments, path: str):
+    def exc(self, blob: datasets.Dataset, path: str):
         """
 
         :param blob:
@@ -35,5 +37,5 @@ class Interface:
             blob=blob, archetype=self.__archetype).exc(model=self.__model)
 
         src.evaluate.measurements.Measurements(
-            originals=originals, predictions=predictions, arguments=arguments).exc(path=path)
+            originals=originals, predictions=predictions, arguments=self.__arguments).exc(path=path)
 
